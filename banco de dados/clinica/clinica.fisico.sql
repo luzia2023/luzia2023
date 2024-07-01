46f127c83166c85613720818cdc7f631626274d1
luzia2023 <150247151+luzia2023@users.noreply.github.com>
2024-07-01 17:34:10 -0300

    add

---
+++ banco de dados/clinica/clinica.fisico.sql
@@ -0,0 +1,1 @@
+Create Database if not exists clinica2;
---
+++ banco de dados/clinica/clinicaPopular.sql
@@ -0,0 +1,6 @@
+use clinica2;
+
+insert into medico( numerocrm, estadocrm, nomemed)
+values (854896,'DF','Jose joão'),
+(54674,'PE', 'Ana Maria');
+
