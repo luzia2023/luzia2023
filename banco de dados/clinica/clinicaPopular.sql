b22a43ec90ad2e9640384aa7786f1bbfb55fe3cd
luzia2023 <150247151+luzia2023@users.noreply.github.com>
2024-07-02 15:36:07 -0300

    Add

--- banco de dados/clinica/clinicaPopular.sql
+++ banco de dados/clinica/clinicaPopular.sql
@@ -1,6 +1,27 @@
+SELECT * FROM clinica2.medico;
 use clinica2;
 
-insert into medico( numerocrm, estadocrm, nomemed)
-values (854896,'DF','Jose joão'),
-(54674,'PE', 'Ana Maria');
 
+
+INSERT INTO paciente(
+nomepaciente,datanascimento, sexo,cep,cidade, estado, complemento)
+values('joão jose', '1980-04-26','M',74846594,'Samamaia','DF', ''),
+	   ('Maria', '1977-03-14','F',748486594,'Samamaia','DF', '');
+       
+
+insert into 
+consulta(
+datahora, numerocrm, estadocrm, idpaciente)
+values ('2024-06-18 17:40:00', '54674','PE','Ana Maria'),
+		('2024-07-02 14:30:00', '54674','PE', 'Ana Maria'),
+        ('2024-08-24  10:30:00','854896','DF', 'jose João'); 
+        
+
+
+insert into especialidade( nomeespecialidade)
+values ('Clinica Médica'),
+	('Fono'),
+    ('Ortopedia'),
+    ('Ginecologia');
+    
+	describe medico;
